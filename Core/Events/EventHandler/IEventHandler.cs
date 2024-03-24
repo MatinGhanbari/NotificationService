7994d1b1eecc4dd573fe649c9a5e64ba59e9ca35
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-03-24 14:41:34 +0330

    Add IEventHandler

---
+++ Core/Events/EventHandler/IEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using Core.Events.EventBus;
+using FluentResults;
+
+namespace Core.Events.EventHandler;
+
+public interface IEventHandler { }
+
+public interface IEventHandler<in TEvent> : IEventHandler where TEvent : BaseEvent
+{
+    Task<Result> Handle(TEvent @event);
+}
