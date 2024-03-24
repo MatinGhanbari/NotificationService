060c3978d2bce6e94b296e492ced6e680981730e
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-03-24 15:03:48 +0330

    [Server]: Update program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,25 +1,31 @@
+using Bootstrapper;
+using ConsumerApplication.GrpcServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add event bus services
+builder.Services.RegisterEventBusServices(builder.Configuration);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add notification services
+builder.Services.AddCorsService()
+                .AddGrpcServices()
+                .RegisterEventHandlers()
+                .AddNotificationService();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
-app.UseAuthorization();
+app.UseGrpcWeb();
+app.MapGrpcService<NotificationService>().EnableGrpcWeb();
 
-app.MapControllers();
+app.Services.SubscribeEvents();
 
 app.Run();
