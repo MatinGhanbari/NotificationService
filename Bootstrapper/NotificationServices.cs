97d6023f40334961d5227ab00d5ef97cc58f6eae
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-03-24 15:01:33 +0330

    [Bootstrapper]: Add NotificationServices

---
+++ Bootstrapper/NotificationServices.cs
@@ -0,0 +1,44 @@
+﻿using ConsumerApplication.Notification.Events;
+using ConsumerApplication.Notification.Handler;
+using Core.Events.EventHandler;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bootstrapper;
+
+public static class NotificationServices
+{
+    public static IServiceCollection AddGrpcServices(this IServiceCollection services)
+    {
+        services.AddGrpc(cfg => cfg.EnableDetailedErrors = true);
+        return services;
+    }
+
+    public static IServiceCollection AddNotificationService(this IServiceCollection services)
+    {
+        services.AddSingleton<ConsumerApplication.GrpcServices.NotificationService>();
+        return services;
+    }
+
+    public static IServiceCollection AddCorsService(this IServiceCollection services)
+    {
+        services.AddCors(cfg =>
+        {
+            cfg.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection RegisterEventHandlers(this IServiceCollection services)
+    {
+        services.AddTransient<NotificationEventHandler>();
+        services.AddTransient<IEventHandler<NotificationEvent>, NotificationEventHandler>();
+
+        return services;
+    }
+}
