1ff73dad84173574bdb0a1550cb0cd652f66f217
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-03-24 14:59:57 +0330

    [ConsumerApplication]: Add NotificationEvent

---
+++ ConsumerApplication/Notification/Events/NotificationEvent.cs
@@ -0,0 +1,11 @@
+﻿using Core.Events.EventBus;
+
+namespace ConsumerApplication.Notification.Events;
+
+[Serializable]
+public class NotificationEvent : BaseEvent
+{
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime? DateTime { get; set; }
+}
