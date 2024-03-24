f56e566abe8bfceae84476813dcd8547afecf716
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-03-24 14:41:26 +0330

    Add IEventBus

---
+++ Core/Events/EventBus/IEventBus.cs
@@ -0,0 +1,10 @@
+﻿using Core.Events.EventHandler;
+
+namespace Core.Events.EventBus;
+
+public interface IEventBus
+{
+    void Publish<T>(T @event) where T : BaseEvent;
+
+    void Subscribe<T, TH>() where T : BaseEvent where TH : IEventHandler<T>;
+}
