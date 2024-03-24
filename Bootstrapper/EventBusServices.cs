a11fdf3e336c2a04ce5515747db9afd3ad510318
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-03-24 15:01:27 +0330

    [Bootstrapper]: Add EventBusServices

---
+++ Bootstrapper/EventBusServices.cs
@@ -0,0 +1,30 @@
+﻿using ConsumerApplication.Notification.Events;
+using ConsumerApplication.Notification.Handler;
+using Core.Events.EventBus;
+using Core.Events.EventBus.RabbitMQ;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bootstrapper;
+
+public static class EventBusServices
+{
+    public static IServiceCollection RegisterEventBusServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<RabbitMQConfig>(configuration.GetSection(nameof(RabbitMQConfig)));
+        services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+        {
+            var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+            return new RabbitMQBus(scopeFactory, configuration.GetValue<RabbitMQConfig>(nameof(RabbitMQConfig))!);
+        });
+
+        return services;
+    }
+
+    public static void SubscribeEvents(this IServiceProvider services)
+    {
+        var eventBus = services.GetRequiredService<IEventBus>();
+
+        eventBus.Subscribe<NotificationEvent, NotificationEventHandler>();
+    }
+}
