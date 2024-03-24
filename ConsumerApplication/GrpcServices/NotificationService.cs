86d236fd7006e18518d734c4f2b68208ea9ff4f0
MatinGhanbari <ghanbarimatin6@gmail.com>
2024-03-24 15:00:45 +0330

    [ConsumerApplication]: Add NotificationService

---
+++ ConsumerApplication/GrpcServices/NotificationService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using static NotificationGrpc.NotificationGrpcService;
+
+namespace ConsumerApplication.GrpcServices;
+
+public class NotificationService : NotificationBaseService
+{
+    private readonly List<IServerStreamWriter<NotificationResponse>> _notificationStreams = new();
+
+    public override async Task GetNotifications(NotificationRequest request, IServerStreamWriter<NotificationResponse> responseStream, ServerCallContext context)
+    {
+        _notificationStreams.Add(responseStream);
+
+        while (!context.CancellationToken.IsCancellationRequested)
+            await Task.Delay(100);
+
+        _notificationStreams.Remove(responseStream);
+    }
+
+    public virtual IReadOnlyList<IServerStreamWriter<NotificationResponse>> GetNotificationStreams()
+    {
+        return _notificationStreams.AsReadOnly();
+    }
+}
